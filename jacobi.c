5cac19c3bc8a9c6b742882295fdac9c2c56f71b8
servustempore <artur.bill@web.de>
2017-02-19 17:37:14 +0100

    removed this added that

--- jacobi.c
+++ jacobi.c
@@ -6,58 +6,7 @@ Jacobi-Iterationsverfahren
 ***********************************************************/
 
 #include <stdio.h>
-#include <math.h>
-#define N 3
-
-void diag(double (*A)[N], double (*D)[N]){
-
-	for(int ii = 0; ii < N; ii++){
-		D[ii][ii] = A[ii][ii];
-	}	
-
-}
-
-void matrixMult(double (*A)[N], double (*B)[N], double (*C)[N]){
-	double sum = 0;
-
-	for(int i = 0; i < N; i++){
-		for(int j = 0; j < N; j++){
-			for(int k = 0; k < N; k++){
-				sum += (A[i][k] * B[k][j]);
-			}
-			C[i][j] = sum;
-			sum = 0;
-		}
-	}
-}
-
-void matVecMult(double (*A)[N], double *B, double *C){
-	double sum = 0;
-
-	for(int i = 0; i < N; i++){
-		for(int j = 0; j < N; j++){
-			sum += (A[i][j] * B[j]);
-		}
-		C[i] = sum;
-		sum = 0;
-	}
-}
-
-double vecLength(double* vec){
-	double sum = 0;
-
-	for(int i = 0; i < N; i++){
-		sum += (vec[i]*vec[i]);
-	}
-
-	return sqrt(sum);
-}
-
-void vecSubtract(double* A,double* B, double* dest){
-	for(int i = 0; i < N; i++){
-		dest[i] = A[i] - B[i];
-	}
-}
+#include <my_functions.h>
 
 int main(void){
 	
@@ -94,12 +43,3 @@ int main(void){
 
 	return 0;
 }
-
-	/*
-	for(int i = 0; i < N; i++){
-		for(int j = 0; j < N; j++){
-			printf("%lf ", A[i][j]);
-		}
-		printf("\n");
-	}
-	*/
